9c9663f1ecd5b538fb181b061f1d34cab732e2e4
Eloy Castillo <ecastillo@fast2ai.com>
2021-11-14 13:37:17 -0400

    smd-beta and deployment react app

--- front-end/server/config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    stripePublishableKey: 'stripePublishableKey',
-    stripeSecretKey: 'stripeSecretKey',
-    awsAccessKeyId: 'awsAccessKeyId',
-    awsSecretAccessKey: 'awsSecretAccessKey',
-    awsBucket: 'awsBucket',
-};
